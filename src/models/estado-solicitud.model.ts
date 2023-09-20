3d7f1d846cf93c4a75148b99ff1bf1406a7de041
Cristian Camilo Osorio <cristian.1701421857@ucaldas.edu.co>
2023-09-20 17:50:33 -0500

    Se crean los modelos de mysql y mongodb

---
+++ src/models/estado-solicitud.model.ts
@@ -0,0 +1,27 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class EstadoSolicitud extends Entity {
+  @property({
+    type: 'number',
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+
+  constructor(data?: Partial<EstadoSolicitud>) {
+    super(data);
+  }
+}
+
+export interface EstadoSolicitudRelations {
+  // describe navigational properties here
+}
+
+export type EstadoSolicitudWithRelations = EstadoSolicitud & EstadoSolicitudRelations;
---
+++ src/models/mascota.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  identificador: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'date',
+  })
+  fechaNacimiento?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  imagen: string;
+
+
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
